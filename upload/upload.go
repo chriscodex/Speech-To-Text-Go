791d3b51bb67c7252161b610da55ba54048926e0
Christian <gonzalo.espinozacadillo@gmail.com>
2022-10-23 17:46:32 -0500

    load file

--- upload/upload.go
+++ upload/upload.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"io/ioutil"
 	"log"
 	"os"
 
@@ -15,4 +16,11 @@ func main() {
 
 	// Import API KEY from .env file
 	API_KEY := os.Getenv("API_KEY")
+
+	// Load file
+	data, err := ioutil.ReadFile("record1.m4a")
+
+	if err != nil {
+		log.Fatal(err)
+	}
 }
