917120aefb33150b2fb1f082595f0e29d196a25b
Christian <gonzalo.espinozacadillo@gmail.com>
2022-10-27 00:40:47 -0500

    uploadUrl defined in main.go

--- main.go
+++ main.go
@@ -2,22 +2,11 @@ package main
 
 import (
 	"fmt"
-	"time"
 
-	"github.com/ChrisCodeX/Speech-To-Text-Go.git/poll"
-	"github.com/ChrisCodeX/Speech-To-Text-Go.git/transcript"
 	"github.com/ChrisCodeX/Speech-To-Text-Go.git/upload"
 )
 
 func main() {
 	uploadURL := upload.GetUploadURL()
 	fmt.Println(uploadURL)
-
-	transcriptId := transcript.GetIdTranscription(uploadURL)
-	fmt.Println(transcriptId)
-
-	time.Sleep(10 * time.Second)
-
-	textTranscripted := poll.GetTextTranscripted(transcriptId)
-	fmt.Println(textTranscripted)
 }
