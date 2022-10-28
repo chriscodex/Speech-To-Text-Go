ea86d7bc598ebd37005d84bb71a826c9a519696c
Christian <gonzalo.espinozacadillo@gmail.com>
2022-10-28 00:44:51 -0500

    getText transcripted

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 
+	"github.com/ChrisCodeX/Speech-To-Text-Go.git/poll"
 	"github.com/ChrisCodeX/Speech-To-Text-Go.git/transcript"
 	"github.com/ChrisCodeX/Speech-To-Text-Go.git/upload"
 )
@@ -12,6 +13,11 @@ func main() {
 	uploadURL := upload.GetUploadURL()
 	fmt.Println(uploadURL)
 
+	// Get the Id of transcription
 	transcriptId := transcript.GetIdTranscription(uploadURL)
 	fmt.Println(transcriptId)
+
+	// Get text transcripted
+	textTranscripted := poll.GetTextTranscripted(transcriptId)
+	fmt.Println(textTranscripted)
 }
