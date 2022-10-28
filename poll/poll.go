ae01ffe04338a239318a397f4bf5f358bea4a21f
Christian <gonzalo.espinozacadillo@gmail.com>
2022-10-27 19:09:28 -0500

    poll converted to a package

--- poll/poll.go
+++ poll/poll.go
@@ -1,4 +1,4 @@
-package main
+package poll
 
 import (
 	"encoding/json"
@@ -10,7 +10,9 @@ import (
 	"github.com/joho/godotenv"
 )
 
-func main() {
+const TRANSCRIPT_URL = "https://api.assemblyai.com/v2/transcript"
+
+func GetText(idTranscript string) interface{} {
 	err := godotenv.Load()
 	if err != nil {
 		log.Fatal("Error loading .env file")
@@ -19,13 +21,12 @@ func main() {
 	// Import API KEY from .env file
 	API_KEY := os.Getenv("API_KEY")
 
-	const TRANSCRIPT_URL = "https://api.assemblyai.com/v2/transcript"
-	const TRANSCRIPT_ID = "r427robbnf-6649-4765-b5db-c0ef3fb13d7f"
-	const POLLING_URL = TRANSCRIPT_URL + "/" + TRANSCRIPT_ID
+	// Build POLLING_URL
+	pollingUrl := TRANSCRIPT_URL + "/" + idTranscript
 
 	client := &http.Client{}
 
-	req, _ := http.NewRequest("GET", POLLING_URL, nil)
+	req, _ := http.NewRequest("GET", pollingUrl, nil)
 	req.Header.Set("content-type", "application/json")
 	req.Header.Set("authorization", API_KEY)
 
@@ -42,5 +43,7 @@ func main() {
 
 	if result["status"] == "completed" {
 		fmt.Println(result["text"])
+		return result["text"]
 	}
+	return nil
 }
